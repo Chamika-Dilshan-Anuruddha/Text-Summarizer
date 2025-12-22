899656223590955acf79ebcc3eeec5f26001fb8d
Chamika-Dilshan-Anuruddha <anuruddhaedcd@gmail.com>
2025-12-22 16:15:37 +0530

    Project file Comleted

--- src/textSummarizer/components/model_trainer.py
+++ src/textSummarizer/components/model_trainer.py
@@ -1,7 +1,7 @@
 from transformers import TrainingArguments, Trainer
 from transformers import DataCollatorForSeq2Seq
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-from datasets import load_dataset, load_from_disk
+from datasets import load_dataset, load_from_disk #type:ignore
 import torch
 import os
 from textSummarizer.entity import ModelTrainerConfig
@@ -21,7 +21,7 @@ class ModelTrainer:
         trainer_args=TrainingArguments(
             output_dir=self.config.root_dir, num_train_epochs=self.config.num_train_epochs, warmup_steps=self.config.warmup_steps,
             per_device_train_batch_size=self.config.per_device_train_batch_size, weight_decay=self.config.weight_decay,
-            logging_steps=self.config.logging_steps ,evaluation_strategy=self.config.evaluation_strategy,
+            logging_steps=self.config.logging_steps ,eval_strategy=self.config.evaluation_strategy,
             eval_steps=self.config.eval_steps,save_steps=self.config.save_steps,gradient_accumulation_steps=self.config.gradient_accumulation_steps,
 
         )
