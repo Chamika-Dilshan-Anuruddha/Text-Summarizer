b74e324cce5b15776293b7ab1703705df7c1fe1d
Chamika-Dilshan-Anuruddha <anuruddhaedcd@gmail.com>
2025-12-22 22:15:42 +0530

    predict pipeline changed

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -7,15 +7,15 @@ class PredictionPipeline:
     def __init__(self):
         self.config=ConfigurationManager().get_model_evaluation_config()
 
-    def prdict(self,text):
-        tokenizer=AutoTokenizer.from_pretrained(self.conig.tokenizer_path)
-        gen_kwargs={"length":0.8, "num_beams":8, "max_length":128}
+    def predict(self,text):
+        tokenizer=AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        gen_kwargs={"num_beams":8, "max_length":128}
 
         pipe=pipeline("summarization",model=self.config.model_path, tokenizer=tokenizer)
         print("Dialogue:")
         print(text)
 
-        output=pipe(text, **gen_kwargs)["summary_text"]
+        output=pipe(text, **gen_kwargs)[0]["summary_text"]
         print("\nModel Summary:")
         print(output)
 
